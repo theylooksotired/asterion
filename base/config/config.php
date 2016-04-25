af182989f8356e3f7a913402dcedaa4f155a1300
Leano Martinet <theylooksotired@gmail.com>
2016-04-25 10:58:42 +0200

    Version 3.0.1

--- base/config/config.php
+++ base/config/config.php
@@ -1,16 +1,28 @@
 <?php
-/**********************************************/
-/**********************************************/
-// Asterion
-// Main configuration file
-// More information on http://www.asterion-cms.com
-/**********************************************/
-/**********************************************/
+/**
+* @file
+*
+* The config.php is the configuration file of the Asterion instance.
+* It has all the constants that are used in the framework and it loads
+* the autoload.php and phpHelper.php files.
+*
+* @author Leano Martinet <info@asterion-cms.com>
+* @package Asterion
+* @version 3.0.1
+*/
 
-// Title of the page
+/**
+* The TITLE constant defines the title of the page.
+* After the installation it is preferable to use the "title" parameter.
+*/
 define('TITLE', 'Base Site');
 
-// Different URLs of the website
+/**
+* The following constants define the paths to the different
+* main folders of the framework.
+* The most important ones are LOCAL_URL and LOCAL_FILE
+* that must point to the public and private files of the website.
+*/
 define('SERVER_URL', 'http://localhost');
 define('BASE_STRING','/asterion/');
 define('LOCAL_URL', SERVER_URL.BASE_STRING);
@@ -20,22 +32,41 @@ define('BASE_FILE', LOCAL_FILE.APP_FOLDER.'/');
 define('APP_URL', LOCAL_URL.'app/');
 define('APP_FILE', LOCAL_FILE.'app/');
 
-// Activate the debug
+/**
+* The DEBUG constant defines if we are in debug mode or not.
+* It it used to show error messages or build the database automatically.
+* When in production it must be set to false.
+*/
 define('DEBUG', true);
 
-// Languages
+/**
+* The LANGUAGES constant defines the languages of the website.
+* They are separated by the character ":", In this version we have only
+* English, French and Spanish available.
+*/
 define('LANGS', 'en:es:fr');
 
-// Session and cookies
+/**
+* The system starts a session with a proper name for the website.
+*/
 define('SESSION_NAME', 'asterion');
 session_start(SESSION_NAME);
+
+/**
+* The COOKIE_TIME constant defines the duration of cookies in the site.
+*/
 define('COOKIE_TIME', 3600000);
 
-// Url rewrites
+/**
+* The following constants are used to check when the system parses the URL.
+*/
 define('ADMIN_URL_STRING', 'admin');
 define('PAGER_URL_STRING', 'page');
 
-// Folders
+/**
+* The following constants are used to define the working paths.
+* They are just shortcuts to certain common used folders in Asterion.
+*/
 define('MODEL_FILE', BASE_FILE.'lib/');
 define('FRAMEWORK_FILE', APP_FILE.'lib/');
 define('ADMIN_URL', LOCAL_URL.ADMIN_URL_STRING.'/');
@@ -44,12 +75,16 @@ define('STOCK_FILE', BASE_FILE.'stock/');
 define('HELPERS_URL', APP_URL.'helpers/');
 define('HELPERS_FILE', APP_FILE.'helpers/');
 
-// Locations of the content objects
+/**
+* Asterion defines where the objects must be checked when using the autoload.
+*/
 $_ENV['locations'][] = MODEL_FILE;
 $_ENV['locations'][] = FRAMEWORK_FILE.'base/';
 $_ENV['locations'][] = FRAMEWORK_FILE.'admin/';
 
-// Database
+/**
+* The following lines define the access to the database.
+*/
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'lucyinthesky');
@@ -59,8 +94,15 @@ define('DB_TYPE', 'mysql');
 define('DB_PREFIX', 'ast_');
 define("PDO_DSN","mysql:host=".DB_SERVER.";port=".DB_PORT.";dbname=".DB_NAME);
 
-// Images
+/**
+* The LOGO constant defines the path to the logo for the website.
+*/
 define('LOGO', BASE_URL.'visual/img/logo.jpg');
+
+/**
+* The following dimension constants are used to create the different
+* versions of the images in Asterion.
+*/
 define('WIDTH_HUGE', 1600);
 define('HEIGHT_MAX_HUGE', 2400);
 define('WIDTH_WEB', 600);
@@ -71,11 +113,20 @@ define('WIDTH_THUMB', 120);
 define('HEIGHT_MAX_THUMB', 120);
 define('WIDTH_SQUARE', 100);
 
-// Email
+/**
+* The EMAIL constant defines the main email for the website.
+* After the installation it is preferable to use the "email" parameter.
+*/
 define('EMAIL', 'info@asterion-cms.com');
 
-// Load the helpers
+/**
+* Asterion defines UTF8 for the internal encoding of the website.
+*/
 mb_internal_encoding('UTF-8');
+
+/**
+* The framework loads the autoload.php and phpHelper.php files.
+*/
 require_once(APP_FILE.'helpers/autoload.php');
 require_once(APP_FILE.'helpers/phpHelper.php');
 
