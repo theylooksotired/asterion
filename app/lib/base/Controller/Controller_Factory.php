fc3f8191899d37c6f2108fac95d9adc72855a8fc
Leano Martinet <theylooksotired@gmail.com>
2016-04-25 13:46:11 +0200

    Version 3.0.1

--- app/lib/base/Controller/Controller_Factory.php
+++ app/lib/base/Controller/Controller_Factory.php
@@ -1,8 +1,22 @@
 <?php
+/**
+* @class Controller_Factory
+*
+* This class is the factory to load the controllers of the content objects.
+*
+* @author Leano Martinet <info@asterion-cms.com>
+* @package Asterion
+* @version 3.0.1
+*/
 class Controller_Factory {
 
+    /**
+    * The main function to load the controller for the content object.
+    * $GET : Array with the loaded $_GET values
+    * $POST : Array with the loaded $_POST values
+    * $FILES : Array with the loaded $_FILES values
+    */
     static function factory($GET=array(), $POST=array(), $FILES=array()) {
-        // Get the right controller for the object
         $type = (isset($GET['type'])) ? $GET['type'] : '';
         $objectController = $type.'_Controller';
         $addLocation = $type.'/'.$objectController.'.php';
@@ -11,8 +25,7 @@ class Controller_Factory {
                 return new $objectController($GET, $POST, $FILES);
             }    
         }
-        header('Location: '.url('error'));
-        exit();
+        throw new Exception('The controller "'.$type.'" does not exist.');
     }
 
 }
